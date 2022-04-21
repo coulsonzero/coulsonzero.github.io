90a6bd83b866aa40c50b01787d9b2c1194a9e02b
coulsonzero <coulson2021@163.com>
2022-04-21 23:20:35 +0800

    update tweenAnimate.js

--- assets/js/tweenAnimate.js
+++ assets/js/tweenAnimate.js
@@ -2,35 +2,35 @@ function tweenAnimate() {
 	/*======= header =========*/
 	TweenMax.from("header", 1, {
 		opacity: 0,
-		delay: 0,
+		delay: .1,
 		y: -80,
 		ease: Expo.easeInOut,
 	})
 
 	TweenMax.staggerFrom("header .navbar li", 1, {
 		opacity: 0,
-		delay: .8,
+		delay: 1,
 		x: -20,
 		ease: Expo.easeInOut,
 	})
 
 	TweenMax.from("header .nav .logo", 1, {
 		opacity: 0,
-		delay: 1.7,
+		delay: 1.5,
 		x: -20,
 		ease: Expo.easeInOut,
 	})
 
 	TweenMax.from("header .icons", 1, {
 		opacity: 0,
-		delay: 2.2,
-		x: -20,
+		delay: 1.8,
+		x: 0,
 		ease: Expo.easeInOut,
 	})
 
 	TweenMax.from("header .nav-toggle", 1, {
 		opacity: 0,
-		delay: 1.7,
+		delay: 1.8,
 		x: -20,
 		ease: Expo.easeInOut,
 	})
@@ -38,45 +38,45 @@ function tweenAnimate() {
 	/*======= home ========*/
 	TweenMax.from(".home .content-name", 1, {
 		opacity: 0,
-		delay: 3,
+		delay: 2,
 		y: 20,
 		ease: Expo.easeInOut,
 	})
 
 	TweenMax.from(".home  p", 1, {
 		opacity: 0,
-		delay: 3.4,
+		delay: 2.4,
 		y: 20,
 		ease: Expo.easeInOut,
 	})
 
 	TweenMax.from(".home .content-btn", 1, {
 		opacity: 0,
-		delay: 3.7,
+		delay: 2.7,
 		y: 20,
 		ease: Expo.easeInOut,
 	})
 
-	TweenMax.from(".home .img-toggle", 1, {
+	TweenMax.from(".home .awwwards", 2, {
 		opacity: 0,
-		delay: 4.6,
-		y: 40,
+		delay: 3.5,
+		x: 120,
 		ease: Expo.easeInOut,
 	})
 
-	TweenMax.staggerFrom(".home .social li", 1, {
-		opacity: 0,
-		delay: 4.2,
-		y: 30,
-		ease: Expo.easeInOut,
-	})
+    TweenMax.staggerFrom(".home .social li", 1, {
+        opacity: 0,
+        delay: 3.2,
+        y: 30,
+        ease: Expo.easeInOut,
+    })
 
-	TweenMax.from(".home .awwwards", 1, {
-		opacity: 0,
-		delay: 3.2,
-		x: 180,
-		ease: Expo.easeInOut,
-	})
+    TweenMax.from(".home .img-toggle", 1, {
+        opacity: 0,
+        delay: 3.6,
+        y: 40,
+        ease: Expo.easeInOut,
+    })
 }
 
 export default tweenAnimate
