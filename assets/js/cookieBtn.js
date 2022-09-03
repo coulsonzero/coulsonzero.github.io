320b30f7812ca217a95eb3b8bd710fdcd0e44945
coulsonzero <coulson2021@163.com>
2022-09-04 01:07:48 +0800

    update

--- assets/js/cookieBtn.js
+++ assets/js/cookieBtn.js
@@ -5,7 +5,8 @@ const cookie_button = document.querySelector(".cookies .ok-btn")
 
 function cookieBtn() {
     cookie_button.addEventListener("click", () => {
-        cookie_container.classList.add("cookie-hidden")
+        // cookie_container.classList.add("cookie-hidden")
+        cookie_container.classList.add("fade-out-bottom")
     })
 }
 
